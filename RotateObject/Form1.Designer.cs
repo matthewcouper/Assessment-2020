175e15f3075b1ae61b63e411b2403e02803a064f
Matthew Couper <51890733+matthewcouper@users.noreply.github.com>
2020-08-04 12:13:04 +1200

    Early build, testing movement and planets

---
+++ RotateObject/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace RotateObject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrSpaceship = new System.Windows.Forms.Timer(this.components);
+            this.PnlGame = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MnuStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmrPlanet = new System.Windows.Forms.Timer(this.components);
+            this.PnlGame.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tmrSpaceship
+            // 
+            this.tmrSpaceship.Interval = 1;
+            this.tmrSpaceship.Tick += new System.EventHandler(this.tmrSpaceship_Tick);
+            // 
+            // PnlGame
+            // 
+            this.PnlGame.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.PnlGame.Controls.Add(this.label2);
+            this.PnlGame.Controls.Add(this.txtName);
+            this.PnlGame.Controls.Add(this.label1);
+            this.PnlGame.Controls.Add(this.menuStrip1);
+            this.PnlGame.Location = new System.Drawing.Point(0, 0);
+            this.PnlGame.Name = "PnlGame";
+            this.PnlGame.Size = new System.Drawing.Size(800, 450);
+            this.PnlGame.TabIndex = 0;
+            this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlGame_Paint);
+            this.PnlGame.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PnlGame_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Orator Std", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 394);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Player";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 418);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Orator Std", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(330, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ROTATE";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MnuStart,
+            this.MnuStop});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MnuStart
+            // 
+            this.MnuStart.Font = new System.Drawing.Font("Orator Std", 8.999999F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MnuStart.Name = "MnuStart";
+            this.MnuStart.Size = new System.Drawing.Size(55, 20);
+            this.MnuStart.Text = "Start";
+            this.MnuStart.Click += new System.EventHandler(this.MnuStart_Click);
+            // 
+            // MnuStop
+            // 
+            this.MnuStop.Font = new System.Drawing.Font("Orator Std", 8.999999F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MnuStop.Name = "MnuStop";
+            this.MnuStop.Size = new System.Drawing.Size(48, 20);
+            this.MnuStop.Text = "Stop";
+            this.MnuStop.Click += new System.EventHandler(this.MnuStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PnlGame);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.PnlGame.ResumeLayout(false);
+            this.PnlGame.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrSpaceship;
+        private System.Windows.Forms.Panel PnlGame;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MnuStart;
+        private System.Windows.Forms.ToolStripMenuItem MnuStop;
+        private System.Windows.Forms.Timer tmrPlanet;
+    }
+}
+
---
+++ RotateObject/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+
+
+namespace RotateObject
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        Graphics g; //declare a graphics object called g so we can draw on the Form
+        Spaceship spaceship = new Spaceship(); //create an instance of the Spaceship Class called spaceship
+
+        bool turnLeft, turnRight, moveUp, moveLeft, moveRight, moveDown;
+
+        string move;
+
+        //declare a list  missiles from the Missile class
+        List<Missile> missiles = new List<Missile>();
+        List<Planet> planets = new List<Planet>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic, null, PnlGame, new object[] { true });
+           
+            for (int i = 0; i < 7; i++)
+            {
+                int displacement = 10 + (i * 70);
+                planets.Add(new Planet(displacement));
+            }
+        }
+
+
+
+
+        private void tmrSpaceship_Tick(object sender, EventArgs e)
+        {
+
+            if (turnRight)
+            {
+                spaceship.rotationAngle += 5;
+            }
+            if (turnLeft)
+                spaceship.rotationAngle -= 5;
+
+            if (moveRight) // if right arrow key pressed
+            {
+                move = "right";
+                spaceship.MoveSpaceship(move);
+            }
+            if (moveLeft) // if left arrow key pressed
+            {
+                move = "left";
+                spaceship.MoveSpaceship(move);
+            }
+
+            if (moveUp) // if right arrow key pressed
+            {
+                move = "up";
+                spaceship.MoveSpaceship(move);
+            }
+            if (moveDown) // if left arrow key pressed
+            {
+                move = "down";
+                spaceship.MoveSpaceship(move);
+            }
+
+            PnlGame.Invalidate();
+
+        }
+
+      
+
+
+
+
+      
+        private void PnlGame_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                missiles.Add(new Missile(spaceship.spaceRec, spaceship.rotationAngle));
+            }
+        }
+
+        private void PnlGame_Paint(object sender, PaintEventArgs e)
+        {
+            //get the graphics used to paint on the Form control
+            g = e.Graphics;
+            //Draw the spaceship
+            spaceship.drawSpaceship(g);
+
+            foreach (Missile m in missiles)
+            {
+                m.drawMissile(g);
+                m.moveMissile(g);
+
+            }
+
+            foreach (Planet p in planets)
+            {
+                p.draw(g);//Draw the planet
+                p.movePlanet(g);//move the planet
+
+                //if the planet reaches the bottom of the form relocate it back to the top
+                if (p.y >= ClientSize.Height)
+                {
+                    p.y = -20;
+                }
+
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { turnLeft = false; }
+            if (e.KeyData == Keys.Right) { turnRight = false; }
+            if (e.KeyData == Keys.W) { moveUp = false; }
+            if (e.KeyData == Keys.A) { moveRight = false; }
+            if (e.KeyData == Keys.S) { moveDown = false; }
+            if (e.KeyData == Keys.D) { moveLeft = false; }
+        }
+
+        private void MnuStart_Click(object sender, EventArgs e)
+        {
+            tmrSpaceship.Enabled = true;
+        }
+
+        private void MnuStop_Click(object sender, EventArgs e)
+        {
+            tmrSpaceship.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Instructions", "Game Instructions");
+            txtName.Focus();
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { turnLeft = true; }
+            if (e.KeyData == Keys.Right) { turnRight = true; }
+            if (e.KeyData == Keys.W) { moveUp = true; }
+            if (e.KeyData == Keys.A) { moveRight = true; }
+            if (e.KeyData == Keys.S) { moveDown = true; }
+            if (e.KeyData == Keys.D) { moveLeft = true; }
+            if (e.KeyData == Keys.E) { missiles.Add(new Missile(spaceship.spaceRec, spaceship.rotationAngle)); }
+        }
+
+    }
+}
